fdcf9c1ef531901b0f3025e991e1fcbac77ab6b8
psikopatrik <psikopat@azet.sk>
2019-04-11 11:12:31 +0100

    Exercise 1

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+print("Printing another line...")
+
+# This is only a comment
+# An "octorphe" is also called a "pound", "hash" or "mesh".
