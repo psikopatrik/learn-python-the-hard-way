156087cb0ca80c71a533853db381a99aa20e01de
psikopatrik <psikopat@azet.sk>
2019-04-14 17:23:32 +0100

    Exercise 17

---
+++ ex17.py
@@ -0,0 +1,27 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print("Copying from {from_file} to {to_file}..."
+      .format(from_file=from_file, to_file=to_file))
+
+in_file = open(from_file)
+in_data = in_file.read()
+# we could do these two on one line too:
+# in_file = open(from_file); in_data = in_file.read()
+
+print("The input file is {data_length} bytes long.".format(data_length=len(in_data)))
+
+print("Does the output file exist? {!r}".format(exists(to_file)))
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(in_data)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()
+
