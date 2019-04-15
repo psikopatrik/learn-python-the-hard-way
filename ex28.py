51cea3937a88658b6d59b38d0eed9403d5f4c783
psikopatrik <psikopat@azet.sk>
2019-04-15 23:16:08 +0100

    Exercise 28

---
+++ ex28.py
@@ -0,0 +1,59 @@
+# 1
+print(f'True and True: {True and True}')  # True
+
+# 2
+print(f'False and True: {False and True}')  # False
+
+# 3
+print(f'1 == 1 and 2 == 1: {1 == 1 and 2 == 1}')  # False
+
+# 4
+print(f'"test" == "test": {"test" == "test"}')  # True
+
+# 5
+print(f'1 == 1 or 2 != 1: {1 == 1 or 2 != 1}')  # True
+
+# 6
+print(f'True and 1 == 1: {True and 1 == 1}')  # True
+
+# 7
+print(f'False and 0 != 0: {False and 0 != 0}')  # False
+
+# 8
+print(f'True or 1 == 1: {True or 1 == 1}')  # True
+
+# 9
+print(f'"test" == "testing": {"test" == "testing"}')  # False
+
+# 10
+print(f'1 != 0 and 2 == 1: {1 != 0 and 2 == 1}')  # False
+
+# 11
+print(f'"test" != "testing": {"test" != "testing"}')  # True
+
+# 12
+print(f'"test" == 1: {"test" == 1}')  # False
+
+# 13
+print(f'not (True and False): {not (True and False)}')  # True
+
+# 14
+print(f'not (1 == 1 and 0 != 1): {not (1 == 1 and 0 != 1)}')  # False
+
+# 15
+print(f'not (10 == 1 or 1000 == 1000): {not (10 == 1 or 1000 == 1000)}')  # False
+
+# 16
+print(f'not (1 != 10 or 3 == 4): {not (1 != 10 or 3 == 4)}')  # False
+
+# 17
+print(f'not ("testing" == "testing" and "Zed" == "Cool Guy"): {not ("testing" == "testing" and "Zed" == "Cool Guy")}')  # True
+
+# 18
+print(f'1 == 1 and not ("testing" == 1 or 1 == 0): {1 == 1 and not ("testing" == 1 or 1 == 0)}')  # True
+
+# 19
+print(f'"chunky" == "bacon" and not (3 == 4 or 3 == 3): {"chunky" == "bacon" and not (3 == 4 or 3 == 3)}')  # False
+
+# 20
+print(f'3 == 3 and not ("testing" == "testing" or "Python" == "Fun"): {3 == 3 and not ("testing" == "testing" or "Python" == "Fun")}')  # False
