b5bdcbeecc987e6b8c6b6ce68b9156d1eb8b0cf8
psikopatrik <psikopat@azet.sk>
2019-04-15 11:41:15 +0100

    Exercise 20

---
+++ ex20.py
@@ -0,0 +1,41 @@
+# $ python3 ex20.py ex20_sample.txt
+
+from sys import argv
+
+script, input_file = argv
+
+
+def print_all(f):
+    print(f.read(), end="")
+
+
+def rewind(f):
+    f.seek(0)
+
+
+def print_a_line(line_count, f):
+    print(line_count, f.readline(), end="")
+
+
+current_file = open(input_file)
+
+print("First let's print the whole file:")
+
+print_all(current_file)
+
+
+print("Now let's rewind, kind of like a tape.\n")
+
+rewind(current_file)
+
+
+print("Let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
